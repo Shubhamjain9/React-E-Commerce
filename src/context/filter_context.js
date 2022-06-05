aa1db7280ee29596666bbe01004eff843176bdef
Shubhamjain9 <shubham9415jain@gmail.com>
2022-06-05 08:11:03 +0530

    sorting and view changes

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -12,13 +12,60 @@ import {
 } from '../actions'
 import { useProductsContext } from './products_context'
 
-const initialState = {}
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+  sort: 'price-lowest',
+  filters: {
+    text: '',
+    category: 'all',
+    company: 'all',
+    colors: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  }
+}
 
 const FilterContext = React.createContext()
 
 export const FilterProvider = ({ children }) => {
+
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: { products } })
+  }, [products]);
+
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS })
+  }, [products, state.sort])
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW })
+  }
+
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
+
+  const updateSort = (evt) => {
+    const name = evt.target.name;
+    const value = evt.target.value;
+    dispatch({ type: UPDATE_SORT, payload: { sort: value } })
+  }
+
+
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{
+      ...state,
+      setGridView,
+      setListView,
+      updateSort
+    }}>
       {children}
     </FilterContext.Provider>
   )
