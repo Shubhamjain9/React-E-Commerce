f8718481144e32fc238f82cd8c891212c7b2bdb6
Shubhamjain9 <shubham9415jain@gmail.com>
2022-06-05 20:57:40 +0530

    checkout page info

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -9,7 +9,7 @@ const CheckoutPage = () => {
   return <main>
     <PageHero title="checkout" />
     <Wrapper className="page">
-      <h1>Checkout here</h1>
+      <h1>Checkout Page in progress...</h1>
     </Wrapper>
   </main>
 }
